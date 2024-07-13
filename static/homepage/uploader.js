f07deb67325a9ecc1c087627590dde6d317fd6b1
andreeNewbie <andree.forwork@gmail.com>
2024-07-13 21:28:27 +0700

    Chỉnh file uploader.js

--- static/homepage/uploader.js
+++ static/homepage/uploader.js
@@ -1,139 +1,175 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const socket = io('http://localhost:3000', {
-        transports: ['websocket'],
-        reconnection: true,
-        reconnectionAttempts: Infinity,
-        reconnectionDelay: 1000,
-        reconnectionDelayMax: 5000,
-        timeout: 20000
-    });
-
-    const token = localStorage.getItem('token');
-    const username = localStorage.getItem('username');
-
-    if (!token) {
-        window.location.href = "static/login/index.html";
-    } else {
-        document.getElementById('username').textContent = username;
+const uploader = function (socket, token, fileId, file, segmentSize, numberOfSegments) {
+    function Uploader() {
+        this.segmentSize = segmentSize;
+        this.file = file;
+        this.fileId = fileId;
+        this.numberOfSegments = numberOfSegments;
+        this.aborted = false;
+        this.uploadedSize = 0;
+        this.progressCache = {};
+        this.activeConnections = {};
+        this.retryQueue = [];
     }
 
-    const newFileButton = document.getElementById('new-file');
-    const uploadModal = document.getElementById('upload-modal');
-    const closeModal = document.getElementsByClassName('close')[0];
-    const uploadForm = document.getElementById('upload-form');
-    const fileTableBody = document.querySelector('#file-table tbody');
-
-    newFileButton.addEventListener('click', () => {
-        uploadModal.style.display = 'block';
-    });
-
-    closeModal.onclick = () => {
-        uploadModal.style.display = 'none';
+    Uploader.prototype.setOptions = function (options = {}) {
+        this.segmentSize = options.segmentSize || this.segmentSize;
     };
 
-    window.onclick = (event) => {
-        if (event.target == uploadModal) {
-            uploadModal.style.display = 'none';
+    Uploader.prototype.setupFile = function (file) {
+        this.file = file;
+    };
+
+    Uploader.prototype.start = function () {
+        if (!this.file) {
+            throw new Error("Can't start uploading: file has not been chosen");
         }
-    };
 
-    uploadForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const formData = new FormData(uploadForm);
-        const file = formData.get('file');
-        const fileId = `${file.name}-${Date.now()}`;
-        const segmentSize = 300 * 1024; // 1MB segments
-        const numberOfSegments = Math.ceil(file.size / segmentSize);
-
-        socket.emit('upload_file_info', {
-            token: token,
-            file_id: fileId,
-            file_name: file.name,
-            number_of_segments: numberOfSegments
-        });
+        const chunksQuantity = this.numberOfSegments;
+        this.chunksQueue = new Array(chunksQuantity).fill().map((_, index) => index).reverse();
+        this.retryQueue = [];
         
-        const uploaderInstance = uploader(socket, token, fileId, file, segmentSize, numberOfSegments);
-        uploaderInstance.send();
-    });
+        this.sendNext();
+    };
 
-    socket.on('upload_response', (data) => {
-        if (data.message === 'File uploaded successfully') {
-            uploadModal.style.display = 'none';
-            alert(data.message);
-            socket.emit('get_files', { token: token });
+    Uploader.prototype.send = function () {
+        this.start();
+    };
+
+    Uploader.prototype.sendNext = function () {
+        if (this.aborted) return;
+
+        if (!this.chunksQueue.length && !this.retryQueue.length) {
+            if (Object.keys(this.activeConnections).length === 0) {
+                this.complete(null);
+            }
+            return;
+        }
+
+        let chunkId;
+        if (this.chunksQueue.length > 0) {
+            chunkId = this.chunksQueue.pop();
+        } else if (this.retryQueue.length > 0) {
+            chunkId = this.retryQueue.pop();
         } else {
-            alert("Error: " + data.message);
+            // Không có phân đoạn nào trong hàng đợi
+            console.log("No chunks left to process.");
+            return;
         }
-    });
-    
-    function fetchFiles() {
-        socket.emit('get_files', { token: token });
-    }
 
-    socket.on('file_list', (files) => {
-        fileTableBody.innerHTML = '';
-        files.forEach(file => {
-            const row = document.createElement('tr');
-            row.innerHTML = `
-                <td>${file.name}</td>
-                <td>${file.owner}</td>
-                <td><button class="download-btn" data-id="${file._id}" title="Download ${file.name}">Download</button></td>
-            `;
-            fileTableBody.appendChild(row);
-        });
+        const sentSize = chunkId * this.segmentSize;
+        const chunk = this.file.slice(sentSize, sentSize + this.segmentSize);
 
-        document.querySelectorAll('.download-btn').forEach(button => {
-            button.addEventListener('click', (event) => {
-                const fileId = event.target.dataset.id;
-                socket.emit('download_file_info', { token: token, file_id: fileId });
+        this.sendChunk(chunk, chunkId)
+            .then(() => {
+                this.sendNext();
+            })
+            .catch((error) => {
+                this.retryQueue.push(chunkId);
+                this.sendNext();
             });
+    };
+
+    Uploader.prototype.sendChunk = function (chunk, id) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                const segmentData = event.target.result;
+                socket.emit('upload_segment', {
+                    token: token,
+                    file_id: this.fileId,
+                    index: id,
+                    data: segmentData
+                });
+
+                socket.once('upload_segment_response', (data) => {
+                    if (data.status === 'ok') {
+                        delete this.activeConnections[id];
+                        resolve();
+                    } else {
+                        reject(new Error('Failed chunk upload'));
+                    }
+                });
+            };
+            reader.onerror = (error) => {
+                reject(error);
+            };
+            reader.readAsArrayBuffer(chunk);
         });
-    });
+    };
 
-    let downloadSegments = {};
-    let fileMeta = {};
-
-    socket.on('download_file_info', (data) => {
-        const { file_name, file_size, number_of_segments, file_id } = data;
-
-        downloadSegments[file_id] = [];
-        fileMeta[file_id] = { filename: file_name, segments: number_of_segments };
-
-        for (let i = 0; i < number_of_segments; i++) {
-            socket.emit('download_segment', { token: token, file_id: file_id, index: i });
-        }
-    });
-
-    socket.on('download_segment', (data) => {
-        const { index, data: segmentData, file_id } = data;
-
-        if (!downloadSegments[file_id]) {
-            downloadSegments[file_id] = [];
+    Uploader.prototype.handleProgress = function (chunkId, event) {
+        if (event.type === "progress" || event.type === "error" || event.type === "abort") {
+            this.progressCache[chunkId] = event.loaded;
         }
 
-        downloadSegments[file_id][index] = segmentData;
-
-        if (downloadSegments[file_id].filter(Boolean).length === fileMeta[file_id].segments) {
-            const blob = new Blob(downloadSegments[file_id]);
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.style.display = 'none';
-            a.href = url;
-            a.download = fileMeta[file_id].filename;
-            document.body.appendChild(a);
-            a.click();
-            window.URL.revokeObjectURL(url);
-
-            delete downloadSegments[file_id];
-            delete fileMeta[file_id];
+        if (event.type === "loadend") {
+            this.uploadedSize += this.progressCache[chunkId] || 0;
+            delete this.progressCache[chunkId];
         }
-    });
 
-    socket.on('download_response', (data) => {
-        if (data.message) {
-            alert(data.message);
+        const inProgress = Object.keys(this.progressCache).reduce((memo, id) => memo += this.progressCache[id], 0);
+        const sentLength = Math.min(this.uploadedSize + inProgress, this.file.size);
+
+        this.onProgress({
+            loaded: sentLength,
+            total: this.file.size
+        });
+    };
+
+    Uploader.prototype.complete = function (error) {
+        if (error && !this.aborted) {
+            this.end(error);
+            return;
         }
-    });
+        setTimeout(() => this.start(), 0);
+        this.end(error);
+    };
 
-    fetchFiles();
-});
+    Uploader.prototype.abort = function () {
+        Object.keys(this.activeConnections).forEach((id) => {
+            this.activeConnections[id].abort();
+        });
+
+        this.aborted = true;
+    };
+
+    Uploader.prototype.on = function (method, callback) {
+        if (typeof callback !== "function") {
+            callback = () => {};
+        }
+
+        this[method] = callback;
+    };
+
+    const multithreadedUploader = new Uploader();
+
+    return {
+        options: function (options) {
+            multithreadedUploader.setOptions(options);
+            return this;
+        },
+        send: function () {
+            multithreadedUploader.start();
+            return this;
+        },
+        continue: function () {
+            multithreadedUploader.sendNext();
+        },
+        onProgress: function (callback) {
+            multithreadedUploader.on("onProgress", callback);
+            return this;
+        },
+        end: function (callback) {
+            multithreadedUploader.on("end", callback);
+            multithreadedUploader.start();
+            return this;
+        },
+        abort: function () {
+            multithreadedUploader.abort();
+        }
+    };
+};
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = uploader;
+}
